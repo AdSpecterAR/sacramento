e6d17ea11822bd9173d959a7c8e8018fdf26502a
John Li <j@peer.com>
2017-09-21 02:07:02 -0700

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component }       from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+}                                 from 'react-router-dom';
+import AdvertiserUploadForm       from './js/components/advertiserUploadForm';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <Route exact path="/" component={AdvertiserUploadForm} />
+      </Router>
+  );
   }
 }
 
