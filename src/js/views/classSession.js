8d199ce0fd798f7377c80a7ab9f605261c1043ce
Jessica <jessicazhangq7@gmail.com>
2018-08-07 13:56:23 -0400

    Change waiting image from url

--- src/js/views/classSession.js
+++ src/js/views/classSession.js
@@ -126,7 +126,7 @@ export default class ClassSession extends Component {
             >
               <FixedAspectRatio ratio={'560:315'}>
                 <div style={{width: '100%', height: '100%'}}>
-                  <img src={thumbnailUrl} width="100%" height="100%" />
+                  <img src="https://i.imgur.com/7pwEANH.png" width="100%" height="100%" />
                 </div>
               </FixedAspectRatio>
             </div>
