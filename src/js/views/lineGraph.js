61d837a1ffc33a124784a419edc0b215e54de2b6
Jessica <jessicazhangq7@gmail.com>
2018-08-14 19:45:43 -0400

    Change add data points to set state

--- src/js/views/lineGraph.js
+++ src/js/views/lineGraph.js
@@ -1,5 +1,6 @@
 import React, {Component}       from "react";
 import Trend                    from 'react-trend'
+import _ from "underscore";
 
 export default class LineGraph extends Component {
 
@@ -17,14 +18,20 @@ export default class LineGraph extends Component {
 
   //adds a random data point from 1 - 10, removes the first point
   addData(){
-    let addedData = Math.round(Math.random() * 10);
-    this.state.data.push(addedData);
-    this.state.data.splice(0, 1);
-    console.log(this.state.data);
+    let addedData = Math.round(Math.random() * 2 - 1);
+
+    let data = _.clone(this.state.data);
+    let length = data.length;
+    data.push(data[length - 1] + addedData);
+    data.splice(0, 1);
+
+    this.setState({
+      data: data
+    })
   }
 
   componentDidMount() {
-    this.interval = setInterval(this.addData, 500);
+    this.interval = setInterval(this.addData, 250);
   }
 
   componentWillUnmount(){
@@ -33,12 +40,14 @@ export default class LineGraph extends Component {
 
   render(){
     console.log(this.refs.lineGraph)
+
     return (
+
       <div ref="lineGraph"
            style={{position: 'absolute',
              bottom: '20px',
              zIndex: '1',
-             width: '100%',
+             width: '75%',
              // overflow: 'hidden'
              // maxHeight: '100px'
            }}>
@@ -46,7 +55,11 @@ export default class LineGraph extends Component {
                autoDraw
                smooth
                strokeWidth={0.75}
-               strokeOpacity={0.5}
+               strokeOpacity={0.75}
+               markerEnd= '<marker id="dot" viewBox="0 0 10 10" refX="5" refY="5"
+                                                                         markerWidth="5" markerHeight="5">
+        <circle cx="5" cy="5" r="5" fill="red" />
+      </marker>'
                gradient={[this.props.colour]}
           // height={100}
           // width={1500}
